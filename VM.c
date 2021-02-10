cd87a0edbee609b42fe698b9dc6a7a25f9dbdfd0
Louis de Granrut <46935569+LouisDeGranrut@users.noreply.github.com>
2021-02-10 22:08:01 +0100

    Update VM.c

--- VM.c
+++ VM.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 
 #define NUM_REGS 4
+
 int registers[NUM_REGS];//set of 4 registers
+int pcounter = 0;//Program Counter
+int running = 1;//This is a boolean, the machine will stop if 0
 
-//The Program
-int program[] = {0x1064,//in register 0, put 100
-			  0x11C8,//in register 1, put 200
-			  0x2201,//add register 0 and 1 in register 2
-			  0x4402,//print what is in register 2
-			  0x3302,//subtract
-			  0x0000};
-//0xCOMMAND + VALUE
-
-int pcounter = 0;//program counter
-
-//returns the content of the program at the line the program counter is pointing on
-int fetch(){
-    return program[pcounter++];    
-}
-
-/* instruction fields */
+//Instructions declaration
 int instrNum = 0;
 int reg1     = 0;
 int reg2     = 0;
 int reg3     = 0;
 int immediate= 0;
 
-/* decode a word */
-void decode( int instruction )
-{
-  instrNum = (instruction & 0xF000) >> 12;
-  reg1     = (instruction & 0xF00 ) >>  8;
-  reg2     = (instruction & 0xF0  ) >>  4;
-  reg3     = (instruction & 0xF   );
-  immediate= (instruction & 0xFF  );
+//The Program:
+//0x + InstructionNumber + Parameter in hex
+int program[] = {
+				0x1064,//in register 0, put 100
+			  	0x11C8,//in register 1, put 200
+			  	0x2201,//add register 0 and 1 in register 2
+			  	0x4402,//print what is in register 2
+			  	0x3201,//subtract 0 and 
+			  	0x4402,//print what is in register 2
+			  	0x0000};//halt
+
+//Returns the content of the program at the line the program counter is pointing on
+int fetch(){
+    return program[pcounter++];    
 }
 
-/* the VM runs until this flag becomes 0 */
-int running = 1;
+//Decode a word
+void decode( int instruction )
+{
+  instrNum  = (instruction & 0xF000) >> 12;
+  reg1      = (instruction & 0xF00 ) >>  8;
+  reg2      = (instruction & 0xF0  ) >>  4;
+  reg3      = (instruction & 0xF   );
+  immediate = (instruction & 0xFF  );
+}
 
-/* evaluate the last decoded instruction */
+//Evaluate the last decoded instruction
 void eval()
 {
   switch( instrNum )
@@ -56,17 +56,17 @@ void eval()
       break;
     case 2:
       /* add */
-      printf( "add r%d r%d r%d\n", reg1, reg2, reg3 );
+      printf( "add  r%d r%d r%d\n", reg1, reg2, reg3 );
       registers[ reg1 ] = registers[ reg2 ] + registers[reg3 ];
       break;
     case 3:
       /* subtract */
-      printf( "sub r%d r%d r%d\n", reg1, reg2, reg3 );
+      printf( "sub  r%d r%d r%d\n", reg1, reg2, reg3 );
       registers[ reg1 ] = registers[ reg2 ] - registers[reg3 ];
       break;
     case 4:
       /* print value in register */
-      printf( "print value in r%d: ", immediate);
+      printf( "show r%d: ", immediate);
       printf("%d\n",registers[ immediate ]);
       break;
     default:
@@ -76,10 +76,10 @@ void eval()
 }
 
 /* display all registers as 4-digit hexadecimal words */
-void showRegisters()
+void showMemory()
 {
   int i;
-  printf( "regs = " );
+  printf( "Registers = " );
   for( i=0; i<NUM_REGS; i++ )
     printf( "%04X ", registers[ i ] );
   printf( "\n" );
@@ -90,12 +90,12 @@ void Run()
 {
   while( running )
   {
-    //showRegisters();
+    //showMemory();
     int instruction = fetch();
     decode( instruction );
     eval();
   }
-  showRegisters();
+  showMemory();
 }
 
 int main( int argc, const char * argv[] )
